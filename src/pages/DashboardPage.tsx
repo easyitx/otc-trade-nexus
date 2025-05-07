cd563307cc531e802ddf187fff391fa9dec13b49
easyitx <vlad_hatkevich@mail.ru>
2025-05-07 08:23:16 +0300

    Update DashboardPage.tsx

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -296,7 +296,7 @@ export default function DashboardPage() {
         </div>
         
         {/* Order Statistics Component */}
-        <OrderStatistics />
+{/*         <OrderStatistics /> */}
         
         {/* Stats Grid - Keep for historical/comparison purposes */}
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
